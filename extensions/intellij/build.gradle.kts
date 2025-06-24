a5d20f88cf0b9726475a2fbb96c1e2499359d371
zhouhongyu <zhouhongyu@yinhai.com>
2025-06-24 15:32:51 +0800

    feat: 更新日志管理依赖1. 添加SLF4J到Log4j2.x的桥接实现，优化日志记录兼容性。

--- extensions/intellij/build.gradle.kts
+++ extensions/intellij/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     // Add Log4j 2.x explicitly
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    // Add SLF4J to Log4j 2.x bridge
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
     testImplementation(kotlin("test"))
 }
 
