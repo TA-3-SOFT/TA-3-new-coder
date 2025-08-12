71e3f5bb469da9f5646dde46bd95b55f2929e257
zhouhongyu <zhouhongyu@yinhai.com>
2025-08-12 14:31:21 +0800

    chore: 更新API地址注释和插件版本号
    1. 在ConfigHandler.ts和ContinueAuthService.kt中添加新的API地址注释
    2.将插件版本从1.0.23-beta升级到1.0.24-beta

--- core/config/ConfigHandler.ts
+++ core/config/ConfigHandler.ts
@@ -139,6 +139,7 @@ export class ConfigHandler {
   private async loadProfiles(org: any) {
     const response = await fetch(
       // "http://192.168.20.195:8081/lowcodeback/ai/continue/ide/list-assistants?organizationId=" +
+      // "http://121.43.97.233:7080/lowcodeback-dev/ai/continue/ide/list-assistants?organizationId=" +
       "https://lc.yinhaiyun.com/lowcodeback-dev/ai/continue/ide/list-assistants?organizationId=" +
         org.id,
       {
@@ -186,6 +187,7 @@ export class ConfigHandler {
   private async loadOrgs() {
     const response = await fetch(
       // "http://192.168.20.195:8081/lowcodeback/ai/continue/ide/list-organizations",
+      // "http://121.43.97.233:7080/lowcodeback-dev/ai/continue/ide/list-organizations",
       "https://lc.yinhaiyun.com/lowcodeback-dev/ai/continue/ide/list-organizations",
       {
         method: "POST",
